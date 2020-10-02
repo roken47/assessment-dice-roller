745982405f18982a72a523f3520b86f660f0062c
roken47 <69013261+roken47@users.noreply.github.com>
2020-10-02 17:56:58 -0500

    confused on maths

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ let numSides = Number(userNumSides);
 // Results
 const totalSpan = document.querySelector("#die-faces-sum");
 const rollResults = document.querySelector("#results");
+let rollResultsList = [];
 // Buttons
 const rollButton = document.querySelector("#roll-button");
 const resultsButton = document.querySelector("#results-button");
@@ -13,9 +14,19 @@ const resetButton = document.querySelector("#reset-button");
 // Dice Array
 // let diceSides = [];
 // let diceRolled = [];
-let dice = [];
+// let dice = [];
 // Roll Button Actions
-
+let incrementRoll = 0;
+rollButton.addEventListener("click", function () {
+  let actionRoll = Math.floor(Math.random() * numSides);
+  while (incrementRoll < numDice) {
+    incrementRoll++;
+    actionRoll;
+    console.log(actionRoll);
+  }
+  console.log(rollResultsList);
+  // if statement for manipulating innerHTML content
+});
 // Show Results of each Die Rolled
 
 // Reset Button Actions
