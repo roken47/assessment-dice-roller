871915fa2c3f4a7cf74e9e64b28528b0aa22a6a9
roken47 <69013261+roken47@users.noreply.github.com>
2020-10-02 15:51:17 -0500

    main.js init layout

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+// User Data Input
+const userNumDice = document.querySelector("#usr-num-dice");
+let numDice = Number(userNumDice);
+const userNumSides = document.querySelector("#usr-num-sides");
+let numSides = Number(userNumSides);
+// Results
+const totalSpan = document.querySelector("#die-faces-sum");
+const rollResults = document.querySelector("#results");
+// Buttons
+const rollButton = document.querySelector("#roll-button");
+const resultsButton = document.querySelector("#results-button");
+const resetButton = document.querySelector("#reset-button");
+// Dice Array
+// let diceSides = [];
+// let diceRolled = [];
+let dice = [];
+// Roll Button Actions
+
+// Show Results of each Die Rolled
+
+// Reset Button Actions
