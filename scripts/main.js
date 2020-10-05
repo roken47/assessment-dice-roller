1bec8c9774e933fcd710b024ac5f837024872c81
roken47 <mradeboer7@gmail.com>
2020-10-04 19:22:28 -0500

    dice-roller CHK results CHK total-sum CHK

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,6 @@
 // User Data Input
 const userNumDice = document.querySelector("#usr-num-dice");
-let numDice = Number(userNumDice);
 const userNumSides = document.querySelector("#usr-num-sides");
-let numSides = Number(userNumSides);
 // Results
 const totalSpan = document.querySelector("#die-faces-sum");
 const rollResults = document.querySelector("#results");
@@ -11,22 +9,46 @@ let rollResultsList = [];
 const rollButton = document.querySelector("#roll-button");
 const resultsButton = document.querySelector("#results-button");
 const resetButton = document.querySelector("#reset-button");
-// Dice Array
-// let diceSides = [];
-// let diceRolled = [];
-// let dice = [];
 // Roll Button Actions
 let incrementRoll = 0;
 rollButton.addEventListener("click", function () {
-  let actionRoll = Math.floor(Math.random() * numSides);
+  let numDice = userNumDice.value;
+  let numSides = userNumSides.value;
+  if (numDice === null || numSides === null) {
+    window.alert("Please Enter '#'");
+  } else if (numDice === null && numSides === null) {
+    window.alert("Please Enter a '#'");
+  }
+  // Google for Testing for Numbers
+  // let minSideValue = 1;
   while (incrementRoll < numDice) {
     incrementRoll++;
+    let actionRoll =
+      // minSideValue + Math.floor(Math.random() * (numSides - minSideValue + 1));
+      1 + Math.floor(Math.random() * numSides);
     actionRoll;
     console.log(actionRoll);
+    rollResultsList.push(actionRoll);
   }
   console.log(rollResultsList);
+  //The reduce() method reduces the array to a single value
+  totalSpan.innerHTML = rollResultsList.reduce((a, b) => a + b, 0);
+  // Sum rolls into totalSpan
   // if statement for manipulating innerHTML content
 });
 // Show Results of each Die Rolled
-
+resultsButton.addEventListener("click", function () {
+  let listAppear = document.querySelector("#results");
+  let rollList = document.createElement("ol");
+  let index = 0;
+  while (index < rollResultsList.length) {
+    let rollValue = document.createElement("li");
+    rollValue.appendChild(document.createTextNode(rollResultsList[index]));
+    rollList.appendChild(rollValue);
+    index++;
+  }
+  listAppear.appendChild(rollList);
+  console.log(rollList);
+});
+// Figure out Resetting above list too, Removing the children
 // Reset Button Actions
