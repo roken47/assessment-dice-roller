cccb8971638541f31748b4ba64d0cd6ddca1f87d
roken47 <mradeboer7@gmail.com>
2020-10-06 09:35:32 -0500

    Good Enough! Lacks proper CSS design

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
+// Best results on comments install 'Better Comments' vscode ext.
 // User Data Input
-document.querySelector("#usr-num-dice").focus();
 const userNumDice = document.querySelector("#usr-num-dice");
 const userNumSides = document.querySelector("#usr-num-sides");
-// const userForm = document.querySelector("#diceRoller");
+//// const userForm = document.querySelector("#diceRoller");
 // Results
 const totalSpan = document.querySelector("#die-faces-sum");
 let rollResultsList = [];
@@ -18,23 +18,39 @@ function rollUpOffMe() {
   let incrementRoll = 0;
   let numDice = userNumDice.value;
   let numSides = userNumSides.value;
-  if (numDice === null || numSides === null) {
+  if (isNaN(numDice) || isNaN(numSides)) {
     window.alert("Please Enter '#'");
-  } else if (numDice === null && numSides === null) {
-    window.alert("Please Enter a '#'");
+    userNumDice.value = "";
+    userNumSides.value = "";
+    return;
   }
-  // !Google for Testing for Numbers
-  // let minSideValue = 1;
+  //! This works but not as intended. I intend to check each and ONLY clear the NaN const.value NOT! *CLEAR ALL*
+  //// else if (numDice === NaN) {
+  // //  userNumDice.value = "";
+  //// } else if (numSides === NaN) {
+  ////   userNumSides.value = "";
+  //// } else if ((numDice && numSides) === NaN) {
+  ////   userNumDice.value = "";
+  ////   userNumSides.value = "";
+  //// }
+  //// else if (isNaN(numSides) || isNaN(numDice)) {
+  ////   window.alert("Please Enter '#'");
+  ////   userNumSides.value = "";
+  ////   userNumDice.value = "";
+  ////   return;
+  //// }
+  // !Google for Testing for Numbers <<DONE>>
+  //// let minSideValue = 1;
   while (incrementRoll < numDice) {
     incrementRoll++;
     let actionRoll =
-      // minSideValue + Math.floor(Math.random() * (numSides - minSideValue + 1));
+      //// minSideValue + Math.floor(Math.random() * (numSides - minSideValue + 1));
       1 + Math.floor(Math.random() * numSides);
     actionRoll;
     // console.log(actionRoll);
     rollResultsList.push(actionRoll);
   }
-  // Sum rolls into totalSpan
+  //// Sum rolls into totalSpan
   // The reduce() method reduces the array to a single value
   totalSpan.innerHTML = rollResultsList.reduce((a, b) => a + b, 0);
 }
@@ -65,7 +81,10 @@ function resetApplet() {
   userNumDice.value = "";
   userNumSides.value = "";
   totalSpan.innerHTML = "__";
-  //! focus on reset isn't working
-  document.querySelector("#usr-num-dice").autofocus = true;
+  //// focus on reset isn't working
 }
+resetButton.addEventListener("click", function () {
+  userNumDice.focus();
+  //? https://developer.mozilla.org/en-US/docs/Web/API/HTMLOrForeignElement/focus
+});
 //?Byron: https://medium.com/@naveenkarippai/learning-how-references-work-in-javascript-a066a4e15600
